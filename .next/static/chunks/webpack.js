f386d46ce35e6cc861c2c15ae29dfbcab077de87
Paula Vaz <paula@guava.com.br>
2020-12-10 11:44:27 -0300

    Adds title page using Head component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6e956fbbfcaf44accf54";
+/******/ 	var hotCurrentHash = "8f0bddb5a45c063e39e4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
